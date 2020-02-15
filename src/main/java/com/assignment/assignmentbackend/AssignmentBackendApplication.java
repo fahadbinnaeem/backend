defd0ef4f9b3183b61305a1650135a9bb3d6e51e
fahad <https://fahad_tplmaps@bitbucket.org>
2020-02-15 18:28:09 +0500

    check tag number

--- src/main/java/com/assignment/assignmentbackend/AssignmentBackendApplication.java
+++ src/main/java/com/assignment/assignmentbackend/AssignmentBackendApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AssignmentBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AssignmentBackendApplication.class, args);
+		SpringApplication.run(AssignmentBackendApplication.class, args); 
 	}
 
 }
